cc3ea96e8cae1c39ec95baa42659ee84902815f6
Đào Thanh Tùng <tung.dao@difisoft.com>
2022-01-13 17:55:00 +0700

    add ipv6 cmd

--- Dockerfile
+++ Dockerfile
@@ -19,6 +19,8 @@ FROM alpine:latest
 COPY --from=build /app/bin/mubeng /bin/mubeng
 ENV HOME /
 COPY ./proxies.txt /proxies.txt
+COPY ./proxies_ipv6.txt /proxies_ipv6.txt
 
-EXPOSE 23528
+EXPOSE 23528 23529
 CMD ["/bin/mubeng", "-a", "127.0.0.1:23528", "-f", "/proxies.txt"]
+# CMD ["/bin/mubeng", "-a", "127.0.0.1:23529", "-f", "/proxies_ipv6.txt"]
--- build_run.sh
+++ build_run.sh
@@ -2,8 +2,16 @@
 
 export dockerImgTag=cascade_proxy_mubeng
 export dockerCtnName=cascade_proxy_mubeng
+export dockerCtnName2=cascade_proxy_mubeng_ipv6
 
 docker build --tag=${dockerImgTag} .
 docker rm -f ${dockerCtnName}
 docker run -dit --name=${dockerCtnName} --restart=always \
-    --network=host ${dockerImgTag}
+    --network=host \
+    ${dockerImgTag}
+
+docker rm -f ${dockerCtnName2}
+docker run -dit --name=${dockerCtnName2} --restart=always \
+    --network=host \
+    ${dockerImgTag} \
+    /bin/mubeng -a 127.0.0.1:23529 -f /proxies_ipv6.txt
